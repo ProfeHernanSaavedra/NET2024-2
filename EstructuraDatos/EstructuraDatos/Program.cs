736acdebab2a778b136ab13c65bed4863f0f023e
hernansaavedrag <hernan.saavedra@gmail.com>
2024-10-23 13:17:12 -0300

    POO 18-10-2024

---
+++ EstructuraDatos/EstructuraDatos/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] dias = new string[5];
+            string [] dias = {"lunes","martes","miercoles","jueves","viernes"};
+            Console.WriteLine(dias[2]);
+
+            //for+tab+tab
+            Console.WriteLine("\nRecorrido con for");
+            for (int i = 0; i < dias.Length; i++)
+            {
+                Console.WriteLine(dias[i]);
+            }
+
+            //fore+tab+tab
+            Console.WriteLine("\nRecorrido con for each");
+            foreach (string dia in dias)
+            {
+                Console.WriteLine(dia);
+            }
+
+            //matrices
+
+            int[,] sala = new int[8, 10];
+            Random rand = new Random();
+            Console.WriteLine("matriz de {0} x {1}",sala.GetLength(0),sala.GetLength(1));
+            Console.WriteLine("La matriz en la posicion 0,0 es: ",sala[0, 0]);
+
+            Console.WriteLine("\nRecorrido con for");
+            for (int f = 0; f < sala.GetLength(0); f++)
+            {
+                for (int c = 0; c < sala.GetLength(1); c++)
+                {
+                    sala[f, c] = rand.Next(0, 2);
+                    Console.Write(sala[f,c] + " ");
+                }
+                Console.WriteLine("");
+            }
+
+            //listas
+
+            Console.WriteLine("\nListas:");
+            List<string> colores = new List<string>();
+            colores.Add("rojo");
+            colores.Add("verde");
+            colores.Add("blanco");
+            colores.Add("negro");
+
+            Console.WriteLine("Primer elemento: " + colores[0]);
+            Console.WriteLine("\nRecorrido con for each");
+
+            foreach (string color in colores)
+            {
+                Console.WriteLine(color);
+            }
+
+            colores.Add("Amarillo");
+
+            Console.WriteLine("\nRecorrido con for each");
+
+            foreach (string color in colores)
+            {
+                Console.WriteLine(color);
+            }
+
+            colores.Insert(0,"rosado");
+
+            Console.WriteLine("\nRecorrido con for each");
+
+            foreach (string color in colores)
+            {
+                Console.WriteLine(color);
+            }
+
+            Console.WriteLine("Total elementos en la lista "+colores.Count);
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ POO/Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logica;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona per = new Persona();
+            per.Nombre = "Maria";
+            per.Apellido = "Perez";
+            per.FechaNacimiento = new DateTime(2002, 11, 21);
+            Console.WriteLine("{0} Tienes {1} años",per.Nombre,per.Edad() );
+            ///Console.WriteLine(per.Nombre+" Tienes " + per.Edad()+" años");
+
+            Console.WriteLine("Hola " + per.Nombre);
+
+            Persona per2 = new Persona("Armando","Casas");
+            per2.FechaNacimiento = new DateTime(2000,10,11);
+            Console.WriteLine("Hola " + per2.Nombre);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
